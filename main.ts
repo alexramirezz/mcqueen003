f4a9e12b868bf801f099b7ba4a1578bb8ffcf275
alexramirezz <106143386+alexramirezz@users.noreply.github.com>
2022-06-14 10:14:17 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,34 @@
 let Izq = 0
 let Der = 0
+input.onSound(DetectedSound.Loud, function () {
+    input.setSoundThreshold(SoundThreshold.Loud, 255)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+})
 basic.forever(function () {
-    Izq = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
-    Der = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
-    if (!(Izq) && Der) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
-    }
-    if (!(Der) && Izq) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
-    }
-    if (!(Der) && !(Izq)) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) > 10) {
+        Izq = maqueen.readPatrol(maqueen.Patrol.PatrolLeft)
+        Der = maqueen.readPatrol(maqueen.Patrol.PatrolRight)
+        if (!(Izq) && Der) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 10)
+        }
+        if (!(Der) && Izq) {
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 10)
+        }
+        if (!(Der) && !(Izq)) {
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+        }
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
     }
 })
 basic.forever(function () {
